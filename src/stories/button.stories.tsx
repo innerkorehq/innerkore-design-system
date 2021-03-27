2291eead1f49f5fb31691886fc0d5a039fd866a5
Gagandeep Singh <gagan@innerkore.com>
2021-03-27 13:27:29 +0530

    Git Hooks & Remaining ESLint warning suppression

--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -11,11 +11,14 @@ export default {
   },
 };
 
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export const primary = (args: ButtonTypes) => {
   return <Button {...args} />;
 };
+
 primary.args = {
   label: 'See now',
   outlined: false,
   onClick: () => action('clicked'),
 };
+/* eslint-enable @typescript-eslint/explicit-module-boundary-types */
