66d4588d6c556e82e24560a8a0d1eae15f69d593
Neelam618 <neelamchavan410@gmail.com>
2021-04-05 16:23:10 +0530

    ButtonWithIcon component added

---
+++ src/components/ButtonWithIcon.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+import ContainedIcon from './click.svg';
+import OutlinedIcon from './click-outlined.svg';
+
+export type ButtonTypes = {
+  /**
+   * Label of the button
+   */
+  label: string;
+  /**
+   * Boolean value to define the button style
+   */
+  outlined?: boolean;
+  /**
+   * Button type
+   */
+  type: string;
+  /**
+   * Button click action
+   */
+  onClick(): void;
+};
+
+const BASE_BUTTON =
+  'rounded outline-none shadow py-3 px-12 font-normal uppercase tracking-wider text-lg';
+const CONTAINED_BUTTON = `${BASE_BUTTON} bg-teal-400 border border-teal-400 text-white`;
+const OUTLINED_BUTTON = `${BASE_BUTTON} border border-teal-400 text-teal-400`;
+
+export const ButtonWithIcon: FC<ButtonTypes> = ({
+  onClick,
+  label = 'Some label',
+  outlined,
+  type,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      type={type ? 'button' : 'submit'}
+      className={outlined ? OUTLINED_BUTTON : CONTAINED_BUTTON}
+    >
+      <img className='inline mr-2 h-6' src={outlined ? OutlinedIcon : ContainedIcon} alt=''/>
+      {label}
+    </button>
+  );
+};
+
+export default ButtonWithIcon;
---
+++ src/stories/ButtonWithIcon.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { ButtonWithIcon, ButtonTypes } from '../components/ButtonWithIcon';
+
+export default {
+  title: 'ButtonWithIcon',
+  component: ButtonWithIcon,
+  argTypes: {
+    label: { control: 'text' },
+    outlined: { control: 'boolean' },
+    onClick: { action: 'clicked' },
+  },
+};
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export const primary = (args: ButtonTypes) => {
+  return <ButtonWithIcon {...args} />;
+};
+
+primary.args = {
+  label: 'Button with icon',
+  outlined: false,
+  //   onClick: () => action('clicked'),
+};
+/* eslint-enable @typescript-eslint/explicit-module-boundary-types */
