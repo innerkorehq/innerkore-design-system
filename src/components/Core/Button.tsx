ead3b20e570f3c293d7171012299f2e845d7efd3
Neelam618 <neelamchavan410@gmail.com>
2021-04-06 18:45:53 +0530

    deleted extra stories and updated button component and its stories

--- src/components/Core/Button.tsx
+++ src/components/Core/Button.tsx
@@ -15,29 +15,32 @@ export type ButtonTypes = {
   size: 'small' | 'medium' | 'large';
 };
 
-const textClasses = 'rounded outline-none shadow py-3 px-12 font-normal tracking-wider text-lg font-bold';
-const normalClassses = `${textClasses} bg-teal-400 border border-teal-400 text-white`;
-const outlinedClasses= `${textClasses} border border-teal-400 text-teal-400`;
+const textClasses = 'rounded outline-none font-normal tracking-wider font-bold';
+const normalClassses = `${textClasses} bg-teal-400 text-white`;
+const outlinedClasses = `${textClasses} border border-teal-400 text-teal-400`;
 const smallClasses = 'text-xs px-3 py-1';
 const mediumClasses = 'text-md px-4 py-2';
 const largeClasses = 'text-xl px-16 py-4';
 
-export const Button: FC<ButtonTypes> = ({ onClick, label = 'Button', type = 'normal', size = 'medium' }) => {
-
+export const Button: FC<ButtonTypes> = ({
+  onClick,
+  label = 'Button',
+  type = 'normal',
+  size = 'medium',
+}) => {
   let fontSize = mediumClasses;
   if (size == 'small') {
     fontSize = smallClasses;
   } else if (size == 'large') {
     fontSize = largeClasses;
-  }
+  } 
 
   let StyleType = normalClassses;
   if (type == 'text') {
     StyleType = textClasses;
-  }
-  else if (type = 'outlined'){
+  } else if (type == 'outlined') {
     StyleType = outlinedClasses;
-  }
+  } 
 
   return (
     <button onClick={onClick} className={`${fontSize} ${StyleType}`}>
---
+++ src/stories/Core/Button.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button, ButtonTypes } from '../../components/Core/Button';
+
+export default {
+  title: 'Core/Button',
+  component: Button,
+  argTypes: {
+    label: { control: 'text' },
+  },
+};
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export const MediumNormal = (args: ButtonTypes) => {
+  return <Button {...args} />;
+};
+
+MediumNormal.args = {
+  label: 'MediumNormal',
+  type: 'normal',
+  size: 'medium'
+};
+/* eslint-enable @typescript-eslint/explicit-module-boundary-types */
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export const SmallNormal = (args: ButtonTypes) => {
+  return <Button {...args} />;
+};
+
+SmallNormal.args = {
+  label: 'SmallNormal',
+  type: 'normal',
+  size: 'small'
+};
+/* eslint-enable @typescript-eslint/explicit-module-boundary-types */
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export const LargeNormal = (args: ButtonTypes) => {
+  return <Button {...args} />;
+};
+
+LargeNormal.args = {
+  label: 'LargeNormal',
+  type: 'normal',
+  size: 'large'
+};
+/* eslint-enable @typescript-eslint/explicit-module-boundary-types */
