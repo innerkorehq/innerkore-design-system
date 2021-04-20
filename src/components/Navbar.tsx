a342d71a995a3f8599d5ac5107aa47c3e1d512f5
Neelam618 <neelamchavan410@gmail.com>
2021-04-20 18:20:47 +0530

    added state for navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Menu } from 'antd';
 
 type MenuItemsType = {
@@ -11,12 +11,21 @@ export type NavbarTypes = {
 };
 
 export const Navbar: FC<NavbarTypes> = ({ MenuItems }) => {
+  const [current, setCurrent] = useState('0');
   return (
     <div className="flex justify-between items-center bg-gray-900 py-2 px-5 md:px-20 xl:px-30">
-      <Menu mode="horizontal" className="bg-gray-900 text-gray-400 border-0">
+      <Menu
+        mode="horizontal"
+        selectedKeys={[current]}
+        className="bg-gray-900 text-gray-400 border-0"
+      >
         {MenuItems.map(({ menuTxt, href }, index) => {
           return (
-            <Menu.Item className="border-0">
+            <Menu.Item
+              key={'key' + index}
+              className={`border-0 ${index + 'key' === current ? 'selectedMenuItem' : 'key'}`}
+              onClick={() => setCurrent('' + index)}
+            >
               <a href={href} className="text-gray-400 hover:text-white text-lg">
                 {menuTxt}
               </a>
