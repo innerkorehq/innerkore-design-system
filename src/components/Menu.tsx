bab194a1cbd7c0e06fcb1a600bd3a36961f0d527
Neelam618 <neelamchavan410@gmail.com>
2021-04-20 13:04:22 +0530

    updated Topmenu component with logo

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 import { Menu } from 'antd';
 import { AppstoreOutlined, createFromIconfontCN } from '@ant-design/icons';
+import Logo from '../../public/img/envato-market-white.png';
 
 const IconFont = createFromIconfontCN({
   scriptUrl: [
     '//at.alicdn.com/t/font_1788044_0dwu4guekcwr.js', // icon-javascript, icon-java, icon-shoppingcart (overrided)
-    '//at.alicdn.com/t/font_1788592_a5xf2bdic3u.js', // icon-shoppingcart, icon-python
   ],
 });
 
@@ -40,24 +40,29 @@ export const TopMenu: FC<TopMenuTypes> = ({
   btnTxt,
 }) => {
   return (
-    <>
-      <Menu mode="horizontal" className="bg-gray-900 text-gray-300">
-        <Menu.Item key="11">Option 11</Menu.Item>
-        <Menu.Item key="12">Option 12</Menu.Item>
-        <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
-          <SubMenu key="sub3" title="Submenu">
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
-        </SubMenu>
-        <Menu.Item key="11">
-          <IconFont type="icon-shoppingcart" />
-          Option 11
-        </Menu.Item>
-      </Menu>
-    </>
+    <div className='flex justify-between items-center bg-gray-900'>
+        <div>
+            <img src={Logo} alt="" className="h-5" />
+        </div>
+        <div>
+            <Menu mode="horizontal" className="bg-gray-900 text-gray-300">
+                <Menu.Item key="11">Option 11</Menu.Item>
+                <Menu.Item key="12">Option 12</Menu.Item>
+                <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
+                    <Menu.Item key="5">Option 5</Menu.Item>
+                    <Menu.Item key="6">Option 6</Menu.Item>
+                    <SubMenu key="sub3" title="Submenu">
+                        <Menu.Item key="7">Option 7</Menu.Item>
+                        <Menu.Item key="8">Option 8</Menu.Item>
+                    </SubMenu>
+                </SubMenu>
+                <Menu.Item key="11">
+                    <IconFont type="icon-shoppingcart" />
+                    Option 11
+                </Menu.Item>
+            </Menu>
+        </div>
+    </div>
   );
 };
 
