10e79b2c0a755b0915bbdc533e15bb6a5620dc68
Neelam618 <neelamchavan410@gmail.com>
2021-04-28 17:59:16 +0530

    card title  added

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { Card } from 'antd';
 import { Row, Col } from 'antd';
+import './Card.css';
 
 // const { Meta } = Card;
 
@@ -11,25 +12,23 @@ type CardType = {
 export const MyCard: FC<MyCardType> = ({
 }) => {   
     return (
-        <div className='border-content'>
+        <div className=''>
 
             <Row gutter={[32, 32]}>
                 <Col lg={8}>
-                    <Card title="Card title" bordered={false}>
+                    <Card className=''>
+                        <a href=""><h1 className='text-4xl text-center hover:underline'>WordPress Themes</h1></a>
+                        
                         <a href='#' className='block relative'>
-
                             <div className='w-16 h-16 mx-auto top-0 rounded-full' style={{boxShadow: '0 0 0 4px white', transform: 'translateY(50%)'}}>
                                 <img src="https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg" alt=""
-                                style={{}}
-                                className='w-16'
+                                className='rounded-full'
                                 />
                             </div>
 
                             <img src="https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png" alt=""
-                            className='border-4 border-white border-solid shadow-lg border-content'
-                            style={{borderRadius: '15px'}}
+                            className='border-4 border-white border-solid shadow-2xl rounded-xl'
                             />
-                            
                         </a>
                     </Card>
                 </Col>
