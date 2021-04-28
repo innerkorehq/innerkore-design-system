23dfa0b06ab84ec01981ee7dfa999291759fe7b3
Neelam618 <neelamchavan410@gmail.com>
2021-04-28 12:18:42 +0530

    svg global alignment

--- src/components/Hero/SearchInput.tsx
+++ src/components/Hero/SearchInput.tsx
@@ -4,19 +4,20 @@ import { Button } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import './SearchInput.css'
 
-
 export type SearchInputType = {
     placeholder: string;
     btnTxt: string;
 }
-        
-export const SearchInput: FC<SearchInputType> = ({placeholder, btnTxt}) => {   
+
+export const SearchInput: FC<SearchInputType> = ({placeholder, btnTxt}) => {  
     return (
         <div id='myInput'>
             <Input 
                 placeholder={placeholder}
                 className='h-20 shadow-lg border-0 rounded-lg text-xl'
-                suffix={<Button type="primary" size='large' className='rounded-md px-5 mr-2' icon={<SearchOutlined  style={{fontSize: 18}}/>}>{btnTxt}</Button>}
+                suffix={<Button type="primary" size='large' className='rounded-md px-5 mr-2' icon={<SearchOutlined  style={{fontSize: 18}}/>}>
+                    <span className='hidden lg:inline-block'>{btnTxt}</span>
+                        </Button>}
                 style={{fontSize: 1}}
             />
         </div>
