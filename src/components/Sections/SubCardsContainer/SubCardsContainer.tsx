4fc69a7063abd1b856f38d3c1f975f3200861d9b
Neelam618 <neelamchavan410@gmail.com>
2021-04-30 19:43:34 +0530

    sub cards section responsive

--- src/components/Sections/SubCardsContainer/SubCardsContainer.tsx
+++ src/components/Sections/SubCardsContainer/SubCardsContainer.tsx
@@ -20,33 +20,40 @@ const { Meta } = Card;
 export const MySubCard: FC<MySubCardType> = ({ MySubCardItems, btnTxt}) => {
     return (
       <div className="bg-gray-200 py-10 lg:py-32">
-          <Row gutter={[32, 32]}>
-              <Col xxl={12} style={{backgroundColor: 'white'}}>
-                    <Row gutter={[24, 24]} style={{ padding: 24, borderRadius: 10, paddingLeft: 80, border: '1px dashed gray'}}>
-                            {MySubCardItems.map(
-                                ({
-                                    imgSrc,
-                                }) => {
-                                return (
-                                    <Col xl={12}>
-                                        <Card
-                                        cover={<a href=''><img alt="example" src={imgSrc} className='relative mx-auto'/></a>}
-                                        bodyStyle={{ background: 'white', position: 'absolute', bottom: 0, left: 0, width: '100%'}}
-                                        >
-                                        <Meta title="Kalles - Clean, Versatile, Responsive Shopify Theme-RTL Support" description="by The4" avatar={<img src={stars} alt='' className='w-40'></img>} />
-                                        </Card>
-                                    </Col>
-                                );
-                                },
-                            )}
-                    </Row>
-              </Col>
-              <Col xxl={12}>
-                  <div className='ml-16 mr-64'>
+          <Row gutter={[32, 32]} style={{marginLeft: 0, marginRight: 0}}>
+            <Col xl={12} className='xl:order-1'>
+                <div className='xl:ml-16 xl:mr-64 text-center xl:text-left'>
                     <h2 className='text-4xl'>Unique themes and templates for every budget and every project.</h2>
-                    <Button type="primary" size='large' className='mt-2 rounded-md'>{btnTxt}</Button>
+                    <Button type="primary" size='large' className='mt-2 rounded-md hidden xl:block'>{btnTxt}</Button>
                 </div>
-              </Col>
+            </Col>
+            <Col xl={12} className='bg-gray-200 xl:bg-white xl:order-0'>
+                <Row gutter={[24, 24]} 
+                style={{ borderRadius: 10 }} className='xl:border border-dashed border-gray-500 pl-0 xl:pl-32 py-5 pr-3'
+                justify= 'center'
+                >
+                        {MySubCardItems.map(
+                            ({
+                                imgSrc,
+                            }) => {
+                            return (
+                                <Col md={12}>
+                                    <Card
+                                    cover={<a href=''><img alt="example" src={imgSrc} className='relative mx-auto'/></a>}
+                                    bodyStyle={{ background: 'white', position: 'absolute', bottom: 0, left: 0, width: '100%'}}
+                                    >
+                                    <Meta title="Kalles - Clean, Versatile, Responsive Shopify Theme-RTL Support" description="by The4" avatar={<img src={stars} alt='' className='w-40'></img>} />
+                                    </Card>
+                                </Col>
+                            );
+                            },
+                        )}
+                </Row>
+            </Col>
+              
+            <Col xl={0} className='mx-auto order-3'>
+                <Button type="primary" size='large' className='mt-2 mx-auto block rounded-md xl:hidden'>{btnTxt}</Button>
+            </Col>
           </Row>
       </div>
     );
