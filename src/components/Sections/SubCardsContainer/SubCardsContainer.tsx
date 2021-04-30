5ad2cdb53be652a03535416464edd15736bfc041
Neelam618 <neelamchavan410@gmail.com>
2021-04-30 17:59:56 +0530

    added sub cards section styles

--- src/components/Sections/SubCardsContainer/SubCardsContainer.tsx
+++ src/components/Sections/SubCardsContainer/SubCardsContainer.tsx
@@ -1,26 +1,58 @@
 import React, { FC } from 'react';
 import { Card } from 'antd';
+import { Row, Col } from 'antd';
+import { Button } from 'antd';
 import stars from '../../../../public/img/stars.jpg';
 import './SubCardsContainer.css';
 
 export type MySubCardType = {
+    MySubCardItems: MySubCardItemsType[];
+    btnTxt: string;
 };
 
 type MySubCardItemsType = {
- 
+    imgSrc: string;
 };
 
 const { Meta } = Card;
 
-export const MySubCard: FC<MySubCardType> = ({}) => { 
-  return (
-       <Card
-        style={{}}
-        cover={<a href=''><img alt="example" src="https://themeforest.img.customer.envatousercontent.com/files/331435158/preview20.jpg?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=f53a15b59e9f7654de4d2d900e954a7d" className='relative mx-auto'/></a>}
-        bodyStyle={{ background: 'white', position: 'absolute', bottom: 0, left: 0, width: '100%'}}
-        >
-        <Meta title="Kalles - Clean, Versatile, Responsive Shopify Theme-RTL Support" description="by The4" avatar={<img src={stars} alt='' className='h-6'></img>} />
-        </Card>
-    
-  );
-};
+
+export const MySubCard: FC<MySubCardType> = ({ MySubCardItems, btnTxt}) => {
+    return (
+      <div className="bg-gray-200 py-10 lg:py-32">
+          <Row gutter={[32, 32]}>
+              <Col xxl={12} style={{backgroundColor: 'white'}}>
+                    <Row gutter={[24, 24]} style={{ padding: 24, borderRadius: 10, paddingLeft: 80, border: '1px dashed gray'}}>
+                            {MySubCardItems.map(
+                                ({
+                                    imgSrc,
+                                }) => {
+                                return (
+                                    <Col xl={12}>
+                                        <Card
+                                        cover={<a href=''><img alt="example" src={imgSrc} className='relative mx-auto'/></a>}
+                                        bodyStyle={{ background: 'white', position: 'absolute', bottom: 0, left: 0, width: '100%'}}
+                                        >
+                                        <Meta title="Kalles - Clean, Versatile, Responsive Shopify Theme-RTL Support" description="by The4" avatar={<img src={stars} alt='' className='w-40'></img>} />
+                                        </Card>
+                                    </Col>
+                                );
+                                },
+                            )}
+                    </Row>
+              </Col>
+              <Col xxl={12}>
+                  <div className='ml-16 mr-64'>
+                    <h2 className='text-4xl'>Unique themes and templates for every budget and every project.</h2>
+                    <Button type="primary" size='large' className='mt-2 rounded-md'>{btnTxt}</Button>
+                </div>
+              </Col>
+          </Row>
+      </div>
+    );
+  };
+  
+
+
+
+
--- src/stories/Sections/SubCardsContainer.stories.tsx
+++ src/stories/Sections/SubCardsContainer.stories.tsx
@@ -9,3 +9,21 @@ export default {
 export const SubCards = (args: MySubCardType) => {
   return <MySubCard {...args} />;
 };
+
+SubCards.args = {
+  MySubCardItems : [
+    {
+      imgSrc: "https://themeforest.img.customer.envatousercontent.com/files/331435158/preview20.jpg?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=f53a15b59e9f7654de4d2d900e954a7d",
+    },
+    {
+      imgSrc: 'https://themeforest.img.customer.envatousercontent.com/files/335174136/0_Preview_big.png?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=fa07ba70bc89d4b589d211fdab649d51',
+    },
+    {
+      imgSrc: 'https://themeforest.img.customer.envatousercontent.com/files/321857191/01_preview.png?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=5a3a602684c453e99ab69be51385205a',
+    },
+    {
+      imgSrc: 'https://themeforest.img.customer.envatousercontent.com/files/335482892/01_preview.png?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=82c0a5a24d0665692743a79105ca5c81',
+    }
+  ],
+  btnTxt: 'View all themes'
+}
