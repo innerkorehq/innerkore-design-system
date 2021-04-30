1687aee3d98c702e829a371eb13e85d59b5f8d58
Neelam618 <neelamchavan410@gmail.com>
2021-04-30 12:40:46 +0530

    added btn under cards

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 import { Card } from 'antd';
 import { Row, Col } from 'antd';
+import { Button } from 'antd';
 import './Cards.css';
 
 export type MyCardType = {
   MyCardItems: MyCardItemsType[];
+  btnTxt: string;
 };
 
 type MyCardItemsType = {
@@ -18,53 +20,67 @@ type MyCardItemsType = {
   iconImgSrc: string;
   cardImgSrc: string;
   cardImgAlt: string;
-}
+};
 
-export const MyCard: FC<MyCardType> = ({ MyCardItems,
-}) => {
+export const MyCard: FC<MyCardType> = ({ MyCardItems, btnTxt }) => {
   return (
     <div className="bg-gray-200 px-5 md:px-20 xl:px-30 py-10 lg:py-32">
       <Row gutter={[32, 32]}>
-        {MyCardItems.map(({titleHref, titleTxt, paraTxt, link1Href, link1Txt, link2Href, link2Txt, iconImgSrc, cardImgSrc, cardImgAlt }) => {
-          return (
-            <Col md={12} xl={8}>
-              <Card
-                hoverable
-                className="bg-gray-100 overflow-hidden shadow-sm hover:shadow-xl border-0 cursor-default hover:bg-white rounded-xl"
-              >
-                <div>
-                  <a href={titleHref}>
-                    <h1 className="text-4xl text-center hover:underline">{titleTxt}</h1>
-                  </a>
-                  <p className="text-center text-lg mb-2">{paraTxt}</p>
-                  <div className="text-center text-lg">
-                    <a href={link1Href} className="mr-5 text-blue-600 hover:underline">
-                      {link1Txt}
+        {MyCardItems.map(
+          ({
+            titleHref,
+            titleTxt,
+            paraTxt,
+            link1Href,
+            link1Txt,
+            link2Href,
+            link2Txt,
+            iconImgSrc,
+            cardImgSrc,
+            cardImgAlt,
+          }) => {
+            return (
+              <Col md={12} xl={8}>
+                <Card
+                  hoverable
+                  className="bg-gray-100 overflow-hidden shadow-sm hover:shadow-xl border-0 cursor-default hover:bg-white rounded-xl"
+                >
+                  <div>
+                    <a href={titleHref}>
+                      <h1 className="text-4xl text-center hover:underline">{titleTxt}</h1>
                     </a>
-                    <a href={link2Href} className="mr-5 text-blue-600 hover:underline">
-                      {link2Txt}
-                    </a>
+                    <p className="text-center text-lg mb-2">{paraTxt}</p>
+                    <div className="text-center text-lg">
+                      <a href={link1Href} className="mr-5 text-blue-600 hover:underline">
+                        {link1Txt}
+                      </a>
+                      <a href={link2Href} className="mr-5 text-blue-600 hover:underline">
+                        {link2Txt}
+                      </a>
+                    </div>
                   </div>
-                </div>
 
-                <a href="#" className="imgWrapper block relative mt-12 -mb-12">
-                  <div
-                    className="w-16 h-16 mx-auto top-0 rounded-full"
-                    style={{ boxShadow: '0 0 0 4px white', transform: 'translateY(50%)' }}
-                  >
-                    <img src={iconImgSrc} alt="" className="rounded-full" />
-                  </div>
-                  <img
-                    src={cardImgSrc}
-                    alt={cardImgAlt}
-                    className="border-4 border-white border-solid shadow-2xl rounded-xl"
-                  />
-                </a>
-              </Card>
-            </Col>
-          );
-        })}
+                  <a href="#" className="imgWrapper block relative mt-12 -mb-12">
+                    <div
+                      className="w-16 h-16 mx-auto top-0 rounded-full"
+                      style={{ boxShadow: '0 0 0 4px white', transform: 'translateY(50%)' }}
+                    >
+                      <img src={iconImgSrc} alt="" className="rounded-full" />
+                    </div>
+                    <img
+                      src={cardImgSrc}
+                      alt={cardImgAlt}
+                      className="border-4 border-white border-solid shadow-2xl rounded-xl"
+                    />
+                  </a>
+                </Card>
+              </Col>
+            );
+          },
+        )}
       </Row>
+      <Button type="primary" size='large' className='mt-16 mx-auto block rounded-md'>{btnTxt}</Button>
+
     </div>
   );
 };
--- src/stories/Cards/Cards.stories.tsx
+++ src/stories/Cards/Cards.stories.tsx
@@ -11,135 +11,133 @@ export const MainCards = (args: MyCardType) => {
 };
 
 MainCards.args = {
-
-    MyCardItems: [
-  {
-    titleHref: '#',
-    titleTxt: 'WordPress Themes',
-    paraTxt: 'Thousands of WordPress themes',
-    link1Href: '#',
-    link1Txt: 'Newest',
-    link2Href: '#',
-    link2Txt: 'BestSellers',
-    iconImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
-    cardImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
-    cardImgAlt: 'WordPress Themes'
-  },
-  {
-    titleHref: '#',
-    titleTxt: 'WordPress Themes',
-    paraTxt: 'Thousands of WordPress themes',
-    link1Href: '#',
-    link1Txt: 'Newest',
-    link2Href: '#',
-    link2Txt: 'BestSellers',
-    iconImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
-    cardImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
-    cardImgAlt: 'WordPress Themes'
-  },
-  {
-    titleHref: '#',
-    titleTxt: 'WordPress Themes',
-    paraTxt: 'Thousands of WordPress themes',
-    link1Href: '#',
-    link1Txt: 'Newest',
-    link2Href: '#',
-    link2Txt: 'BestSellers',
-    iconImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
-    cardImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
-    cardImgAlt: 'WordPress Themes'
-  },
-  {
-    titleHref: '#',
-    titleTxt: 'WordPress Themes',
-    paraTxt: 'Thousands of WordPress themes',
-    link1Href: '#',
-    link1Txt: 'Newest',
-    link2Href: '#',
-    link2Txt: 'BestSellers',
-    iconImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
-    cardImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
-    cardImgAlt: 'WordPress Themes'
-  },
-  {
-    titleHref: '#',
-    titleTxt: 'WordPress Themes',
-    paraTxt: 'Thousands of WordPress themes',
-    link1Href: '#',
-    link1Txt: 'Newest',
-    link2Href: '#',
-    link2Txt: 'BestSellers',
-    iconImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
-    cardImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
-    cardImgAlt: 'WordPress Themes'
-  },
-  {
-    titleHref: '#',
-    titleTxt: 'WordPress Themes',
-    paraTxt: 'Thousands of WordPress themes',
-    link1Href: '#',
-    link1Txt: 'Newest',
-    link2Href: '#',
-    link2Txt: 'BestSellers',
-    iconImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
-    cardImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
-    cardImgAlt: 'WordPress Themes'
-  },
-  {
-    titleHref: '#',
-    titleTxt: 'WordPress Themes',
-    paraTxt: 'Thousands of WordPress themes',
-    link1Href: '#',
-    link1Txt: 'Newest',
-    link2Href: '#',
-    link2Txt: 'BestSellers',
-    iconImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
-    cardImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
-    cardImgAlt: 'WordPress Themes'
-  },
-  {
-    titleHref: '#',
-    titleTxt: 'WordPress Themes',
-    paraTxt: 'Thousands of WordPress themes',
-    link1Href: '#',
-    link1Txt: 'Newest',
-    link2Href: '#',
-    link2Txt: 'BestSellers',
-    iconImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
-    cardImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
-    cardImgAlt: 'WordPress Themes'
-  },
-  {
-    titleHref: '#',
-    titleTxt: 'WordPress Themes',
-    paraTxt: 'Thousands of WordPress themes',
-    link1Href: '#',
-    link1Txt: 'Newest',
-    link2Href: '#',
-    link2Txt: 'BestSellers',
-    iconImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
-    cardImgSrc:
-      'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
-    cardImgAlt: 'WordPress Themes'
-  },
-
-  ]
- 
+  MyCardItems: [
+    {
+      titleHref: '#',
+      titleTxt: 'WordPress Themes',
+      paraTxt: 'Thousands of WordPress themes',
+      link1Href: '#',
+      link1Txt: 'Newest',
+      link2Href: '#',
+      link2Txt: 'BestSellers',
+      iconImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
+      cardImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
+      cardImgAlt: 'WordPress Themes',
+    },
+    {
+      titleHref: '#',
+      titleTxt: 'WordPress Themes',
+      paraTxt: 'Thousands of WordPress themes',
+      link1Href: '#',
+      link1Txt: 'Newest',
+      link2Href: '#',
+      link2Txt: 'BestSellers',
+      iconImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
+      cardImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
+      cardImgAlt: 'WordPress Themes',
+    },
+    {
+      titleHref: '#',
+      titleTxt: 'WordPress Themes',
+      paraTxt: 'Thousands of WordPress themes',
+      link1Href: '#',
+      link1Txt: 'Newest',
+      link2Href: '#',
+      link2Txt: 'BestSellers',
+      iconImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
+      cardImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
+      cardImgAlt: 'WordPress Themes',
+    },
+    {
+      titleHref: '#',
+      titleTxt: 'WordPress Themes',
+      paraTxt: 'Thousands of WordPress themes',
+      link1Href: '#',
+      link1Txt: 'Newest',
+      link2Href: '#',
+      link2Txt: 'BestSellers',
+      iconImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
+      cardImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
+      cardImgAlt: 'WordPress Themes',
+    },
+    {
+      titleHref: '#',
+      titleTxt: 'WordPress Themes',
+      paraTxt: 'Thousands of WordPress themes',
+      link1Href: '#',
+      link1Txt: 'Newest',
+      link2Href: '#',
+      link2Txt: 'BestSellers',
+      iconImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
+      cardImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
+      cardImgAlt: 'WordPress Themes',
+    },
+    {
+      titleHref: '#',
+      titleTxt: 'WordPress Themes',
+      paraTxt: 'Thousands of WordPress themes',
+      link1Href: '#',
+      link1Txt: 'Newest',
+      link2Href: '#',
+      link2Txt: 'BestSellers',
+      iconImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
+      cardImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
+      cardImgAlt: 'WordPress Themes',
+    },
+    {
+      titleHref: '#',
+      titleTxt: 'WordPress Themes',
+      paraTxt: 'Thousands of WordPress themes',
+      link1Href: '#',
+      link1Txt: 'Newest',
+      link2Href: '#',
+      link2Txt: 'BestSellers',
+      iconImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
+      cardImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
+      cardImgAlt: 'WordPress Themes',
+    },
+    {
+      titleHref: '#',
+      titleTxt: 'WordPress Themes',
+      paraTxt: 'Thousands of WordPress themes',
+      link1Href: '#',
+      link1Txt: 'Newest',
+      link2Href: '#',
+      link2Txt: 'BestSellers',
+      iconImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
+      cardImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
+      cardImgAlt: 'WordPress Themes',
+    },
+    {
+      titleHref: '#',
+      titleTxt: 'WordPress Themes',
+      paraTxt: 'Thousands of WordPress themes',
+      link1Href: '#',
+      link1Txt: 'Newest',
+      link2Href: '#',
+      link2Txt: 'BestSellers',
+      iconImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/category-icons/themeforest/wordpress-38017feb3efbc9d223cfb8472fba3cb8.svg',
+      cardImgSrc:
+        'https://assets.market-storefront.envato.com/storefront/packs/media/images/home/themeforest/category-tiles-wordpress-c903c0a5acb77a9e92d5a7db0dbe2565.png',
+      cardImgAlt: 'WordPress Themes',
+    },
+  ],
+  btnTxt: 'View All Categories'
 };
